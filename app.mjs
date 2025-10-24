148061fa309936b61aa9efc323b12c045bbe9f92
fernando <galindez539@gmail.com>
2025-10-24 11:35:31 -0300

    Priimer commit

---
+++ app.mjs
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app=express();
+const PORT=3000;
+
+app.get("/",(req,res)=>{
+    res.send("Servidor funcionando");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
+})
