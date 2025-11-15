9a3c7c4f50714b2358187c624de2488d0f73fc93
Mario Fernando <galindez539@gmail.com>
2025-11-14 21:53:06 -0300

    Ýa todo implementado pero con pruebas necesarias

--- validations/validationsRules.mjs
+++ validations/validationsRules.mjs
@@ -1,31 +1,31 @@
+import mongoose from "mongoose";
 import { body, param } from 'express-validator';
 
 export const CrearheroesValidationRules = ()=>[
     body('nombreSuperheroe')
     .exists()
-    .notEmpty().withMessage("Nombre no válido, asegurese llenar el campo mínimo 3 caracteres")
+    .notEmpty().withMessage("Introduzca un nombre de superheroe")
     .trim()
-    .isLength({min: 3, max: 60}),
+    .isLength({min: 3, max: 60}).withMessage("El nombre de superheroe debe estar entre 3-60 carácteres"),
 
     body('nombreReal')
     .exists()
-    .notEmpty()
+    .notEmpty().withMessage("Introduzca el nombre real del superheroe")
     .trim()
-    .isLength({min: 3}, {max:60}),
+    .isLength({min: 3}, {max:60}).withMessage("El nombre real debe estar entre 3-60 carácteres"),
 
     body('edad')
     .trim()
-    .isInt({min: 0}),
+    .isInt({min: 0}).withMessage("La edad del heroe debe ser mayor a 1"),
 
     body('poderes')
     .exists()
     .notEmpty()
-    .trim()
     .isArray({min: 1})
     .custom(poderes=>{
-        if(!poderes.every(poder => isNaN(poder))){
-            throw new Error('Todos los elementos deben ser strings');
-        }
+        if(!poderes.every(poder => typeof poder === 'string')){
+            throw new Error('Cada poder debe ser un string');
+        };
         return true;
     })
 ]
@@ -48,4 +48,13 @@ export const ValidacionActualizarheroe = ()=>[
     .trim()
 ]
 
+export const ValidacionEliminarPorId = ()=>[
+    
+]
 
+export const ValidacionEliminarPorNombre = ()=>[
+    param('nombre')
+    .exists()
+    .notEmpty()
+    .trim()
+    .isLength({min: 3, max:60})]
